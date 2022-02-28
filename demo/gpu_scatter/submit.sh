d4b271959132f5458bd194897133739662bf1841
Chris Richardson <chris@bpi.cam.ac.uk>
2022-02-28 12:20:55 +0000

    Add Baskerville submit script

---
+++ demo/gpu_scatter/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=mhdz1996-wavefenics
+#SBATCH --reservation=mhdz1996-wavefenics
+#SBATCH --tasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=fenics-gpu
+#SBATCH --time=0:30:0
+#SBATCH --qos=epsrc
+
+module purge
+module load baskerville
+module load OpenMPI
+
+nsys profile --trace=cuda,mpi mpirun -n 4 ./planar3d --size=100 --degree=4
+
+
