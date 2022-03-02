8b6ddcb8087444b7a1343f2ea227ae2c6b48f2ce
Athena Elafrou <ae488@cam.ac.uk>
2022-03-02 21:50:28 +0000

    Added scatter_rev

--- demo/gpu_scatter_mpi/main.cpp
+++ demo/gpu_scatter_mpi/main.cpp
@@ -47,6 +47,7 @@ int main(int argc, char* argv[])
     // MPI
     MPI_Comm mpi_comm{MPI_COMM_WORLD};
     int rank = utils::set_device(mpi_comm);
+    MPI_Datatype data_type = dolfinx::MPI::mpi_type<double>();
 
     // Read mesh and mesh tags
     std::array<std::array<double, 3>, 2> p
@@ -103,7 +104,7 @@ int main(int argc, char* argv[])
     cuda::array<double> d_send_buffer(indices.size());
     cuda::array<double> d_recv_buffer(displs_recv_fwd.back());
 
-    // Find my neighbors
+    // Find my neighbors in forward communicator
     MPI_Comm comm
         = x.map()->comm(dolfinx::common::IndexMap::Direction::forward);
     int num_recv_neighbors, num_send_neighbors;
@@ -116,6 +117,18 @@ int main(int argc, char* argv[])
                              nullptr, num_send_neighbors, send_neighbors.data(),
                              nullptr);
 
+    // Find my neighbors in reverse communicator
+    MPI_Comm rev_comm
+        = x.map()->comm(dolfinx::common::IndexMap::Direction::reverse);
+    int num_rev_recv_neighbors, num_rev_send_neighbors;
+    MPI_Dist_graph_neighbors_count(rev_comm, &num_rev_recv_neighbors,
+                                   &num_rev_send_neighbors, &weighted);
+    std::vector<int> rev_recv_neighbors(num_rev_recv_neighbors);
+    std::vector<int> rev_send_neighbors(num_rev_send_neighbors);
+    MPI_Dist_graph_neighbors(
+        rev_comm, num_rev_recv_neighbors, rev_recv_neighbors.data(), nullptr,
+        num_rev_send_neighbors, rev_send_neighbors.data(), nullptr);
+
     // Prefetch data to gpu
     linalg::prefetch(rank, x);
 
@@ -125,32 +138,70 @@ int main(int argc, char* argv[])
     // Start profiling
     cudaProfilerStart();
 
+    // --------------------------- Phase 1 ---------------------------
     // Scatter forward (owner to ghost -> one to many map)
-    xtl::span<const double> local_data = x.array();
-    gather(indices.size(), d_indices.data(), local_data.data(),
+    // _buffer_send_fwd is send_buffer
+    // _buffer_recv_fwd is recv_buffer
+
+    // Step 1: pack send buffer
+    xtl::span<const double> x_local_const = x.array();
+    gather(indices.size(), d_indices.data(), x_local_const.data(),
            d_send_buffer.data(), block_size);
 
-    // Start send/receive
-    MPI_Request* req = new MPI_Request[num_recv_neighbors];
+    // Step 2: begin scatter
+    std::vector<MPI_Request> req = (num_recv_neighbors);
     for (int i = 0; i < num_recv_neighbors; ++i) {
       MPI_Irecv(d_recv_buffer.data() + displs_recv_fwd[i],
-                sizes_recv_fwd[i + 1], dolfinx::MPI::mpi_type<double>(),
-                recv_neighbors[i], 0, comm, &(req[i]));
+                sizes_recv_fwd[i + 1], data_type, recv_neighbors[i], 0, comm,
+                &(req[i]));
     }
 
     for (int i = 0; i < num_send_neighbors; ++i) {
       MPI_Send(d_send_buffer.data() + displs_send_fwd[i], sizes_send_fwd[i + 1],
-               dolfinx::MPI::mpi_type<double>(), send_neighbors[i], 0, comm);
+               data_type, send_neighbors[i], 0, comm);
     }
 
-    MPI_Waitall(num_recv_neighbors, req, MPI_STATUSES_IGNORE);
+    MPI_Waitall(num_recv_neighbors, req.data(), MPI_STATUSES_IGNORE);
 
-    // Copy into ghost area ("remote_data")
-    xtl::span<double> remote_data(x.mutable_array().data()
-                                      + x.map()->size_local(),
-                                  x.map()->num_ghosts());
+    // Step3: copy into ghost area
+    xtl::span<double> x_remote(x.mutable_array().data() + x.map()->size_local(),
+                               x.map()->num_ghosts());
     gather(ghost_pos_recv_fwd.size(), d_ghost_pos_recv_fwd.data(),
            d_recv_buffer.data(), remote_data.data(), block_size);
+
+    // --------------------------- Phase 2 ---------------------------
+    // Scatter reverse (ghosts to owners -> many to one map)
+    // _buffer_recv_fwd is send_buffer
+    // _buffer_send_fwd is recv_buffer
+    // So swap send_buffer and recv_buffer from scatter_fwd
+
+    // Step 1: pack send buffer
+    xtl::span<const double> x_remote_const(
+        x.array().data() + x.map->size_local(), x.map()->num_ghosts());
+    // FIXME without atomics
+    scatter(d_indices.size(), d_ghost_pos_recv_fwd, x_remote_const,
+            d_recv_buffer, block_size);
+
+    // Step 2: begin scatter
+    req.clear();
+    req.resize(num_rev_recv_neighbors);
+    for (int i = 0; i < num_rev_recv_neighbors; ++i) {
+      MPI_Irecv(d_send_buffer.data() + displs_send_fwd[i],
+                sizes_send_fwd[i + 1], data_type, rev_recv_neighbors[i], 0,
+                rev_comm, &(req[i]));
+    }
+
+    for (int i = 0; i < num_rev_send_neighbors; ++i) {
+      MPI_Send(d_recv_buffer.data() + displs_recv_fwd[i], sizes_recv_fwd[i + 1],
+               data_type, rev_send_neighbors[i], 0, rev_comm);
+    }
+
+    MPI_Waitall(num_rev_recv_neighbors, req.data(), MPI_STATUSES_IGNORE);
+
+    // Step 3: copy/accumulate into owned part of the vector
+    xtl::span<double> x_local(x.mutable_array().data());
+    scatter(shared_indices.size(), d_indices, d_send_buffer, xlocal.data(),
+            block_size);
 
     // End profiling
     cudaProfilerStop();
