489159ac814e6a6e71e6ad76ee1bbb35ba5e3047
IgorBaratta <igorbaratta@gmail.com>
2022-03-08 12:18:12 +0000

    add cpu checks

--- demo/gpu_operator_monolithic/main.cpp
+++ demo/gpu_operator_monolithic/main.cpp
@@ -17,6 +17,7 @@
 #include <cuda/scatter.hpp>
 #include <cuda/transform.hpp>
 #include <cuda/utils.hpp>
+#include <operators.hpp>
 
 using namespace dolfinx;
 namespace po = boost::program_options;
@@ -30,7 +31,8 @@ int main(int argc, char* argv[]) {
   po::options_description desc("Allowed options");
   desc.add_options()("help,h", "print usage message")(
       "size", po::value<std::size_t>()->default_value(32))(
-      "degree", po::value<int>()->default_value(1));
+      "degree", po::value<int>()->default_value(1))(
+      "check", po::value<bool>()->default_value(false));
 
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(desc).allow_unregistered().run(),
@@ -44,6 +46,7 @@ int main(int argc, char* argv[]) {
 
   const std::size_t Nx = vm["size"].as<std::size_t>();
   const int degree = vm["degree"].as<int>();
+  const bool check = vm["check"].as<bool>();
 
   common::subsystem::init_logging(argc, argv);
   common::subsystem::init_mpi(argc, argv);
@@ -65,7 +68,7 @@ int main(int argc, char* argv[]) {
     // Create a Basix continuous Lagrange element of given degree
     basix::FiniteElement e = basix::element::create_lagrange(
         mesh::cell_type_to_basix_type(mesh::CellType::hexahedron), degree,
-        basix::element::lagrange_variant::equispaced, false);
+        basix::element::lagrange_variant::gll_warped, false);
 
     // Create a scalar function space
     std::shared_ptr<fem::FunctionSpace> V
@@ -88,12 +91,30 @@ int main(int argc, char* argv[]) {
     linalg::prefetch(0, x);
     linalg::prefetch(0, y);
 
-    auto quad = basix::quadrature::type::gauss_jacobi;
-    MassOperator<double> op(V, e, quad, 2 * degree);
+    auto quad = basix::quadrature::type::gll;
+    MassOperator<double> op(V, e, quad, 3);
 
     double t = MPI_Wtime();
     op.apply(x, y);
     t = MPI_Wtime() - t;
+
+    if (check) {
+      la::Vector<double> x(idxmap, 1);
+      la::Vector<double> y1(idxmap, 1);
+      std::fill(x.mutable_array().begin(), x.mutable_array().end(), 1);
+      MassOperatorCPU<double> cpu_op(V, 2);
+      double t = MPI_Wtime();
+      cpu_op(x, y1);
+      t = MPI_Wtime() - t;
+      std::cout << "Y norm: " << y1.norm() << std::endl;
+
+      for (int i = 0; i < x.array().size(); i++) {
+        double err = y1.array()[i] - y.array()[i];
+        if (std::abs(err) > 1e-8)
+          std::cout << y1.array()[i] - y.array()[i] << " ";
+      }
+      std::cout << "\n#Elapsed Time: " << t << std::endl;
+    }
 
     std::cout << "X norm: " << x.norm() << std::endl;
     std::cout << "Y norm: " << y.norm() << std::endl;
